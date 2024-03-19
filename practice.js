9cdaf7dad83d1c7293c2cc57d01242b604b62023
Melchs40 <samscodingjourney@gmail.com>
2024-03-19 11:48:16 -0500

    Create cipher function and successful test in practice.js, practice.test.js

--- practice.js
+++ practice.js
@@ -25,3 +25,26 @@ calc.multiply = function multiply(a, b) {
 calc.divide = function divide(a, b) {
   return a / b;
 };
+
+export function cipher(code) {
+  const alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'.split(
+    ''
+  );
+  let input = code.split('');
+  let cipher = '';
+  for (let i = 0; i < input.length; i++) {
+    let position = alphabet.indexOf(input[i]);
+    if (!alphabet.includes(input[i])) {
+      cipher = cipher.concat(input[i]);
+    } else if (position >= alphabet.length - 6) {
+      let newPosition = position - 46;
+      cipher = cipher.concat(alphabet[newPosition]);
+    } else {
+      let newPosition = position + 6;
+      cipher = cipher.concat(alphabet[newPosition]);
+    }
+  }
+  return cipher;
+}
+
+export function analyzeArray(array) {}
--- practice.test.js
+++ practice.test.js
@@ -1,4 +1,4 @@
-import { capital, reverse, calc } from './practice';
+import { capital, reverse, calc, cipher, analyzeArray } from './practice';
 
 test('capitalizes hello to Hello', () => {
   expect(capital('hello')).toBe('Hello');
@@ -13,4 +13,20 @@ test('calculates various things', () => {
   expect(calc.subtract(6, 2)).toBe(4);
   expect(calc.multiply(6, 2)).toBe(12);
   expect(calc.divide(6, 2)).toBe(3);
+});
+
+test('changes Hello! to Khoor!', () => {
+  expect(cipher('Hello!')).toBe('Khoor!');
+});
+
+test('analyze array to show min, max, average, and length of array', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe(
+    object ==
+      {
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6,
+      }
+  );
 });
